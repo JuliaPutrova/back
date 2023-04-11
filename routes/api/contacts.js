1ffe6d640b895122fa26201e0204abebff5a1ca4
JuliaPutrova <Julia.putrova@gmail.com>
2023-04-11 20:47:31 +0300

    cyclic

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json("<h1>My back</h1>");
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message1" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message2" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message3" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({ message: "template message4" });
 });
 
 module.exports = router;
